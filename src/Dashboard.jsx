a645aa13a39e4ae291014419b90de77ff0ca81e8
Veeanty <galihcatur960@gmail.com~>
2024-06-03 13:17:20 +0700

    Footer

---
+++ src/Dashboard.jsx
@@ -0,0 +1,15 @@
+import './Dashboard.css';
+import Footer from './Footer';
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* Konten lain dari dashboard bisa dimasukkan di sini */}
+      
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+}
+
+export default Dashboard;
